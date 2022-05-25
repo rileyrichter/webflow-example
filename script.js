512d5e18789e31a262efa0b08d59de42a1d0a359
rileyrichter <ben@webflow.com>
2022-05-25 11:35:51 -0500

    Adding my script file

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("hey!");
+let root = document.getElementById("root");
+const newText = document.createElement("p");
+newText.innerHTML = "This is a new paragraph!";
+root.appendChild(newText);
